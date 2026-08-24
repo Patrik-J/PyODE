9524070d257b61fc7cd31dbbe006db71c486c4bd
jelicpat <patrik.jelic@uni-a.de>
2026-08-24 19:19:51 +0200

    Simple class for ODEs

---
+++ src/ode/ode.hpp
@@ -0,0 +1,25 @@
+#ifndef ODE_HPP
+#define ODE_HPP
+
+#include <functional>
+#include <vector>
+
+#include "more_math.hpp"
+
+class ODE {
+    /*
+        A basic class to hold one-dimensional ODEs.
+    */
+    public:
+        ODE(std::function<double(double, std::vector<double>)> func);
+
+        // this returns a vector of the same size as the input vector
+        // the entries i = 0,...,n-1 are simply data y^(i+1)_n
+        // the last entry of the vector is f(t_n, y_n)
+        std::vector<double> operator() (double t, std::vector<double> y);
+
+    private:
+        const std::function<double(double, std::vector<double>)> func;
+};
+
+#endif
