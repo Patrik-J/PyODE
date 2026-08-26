690adb478f5e6239216ef122b0c62f008c1fc89d
jelicpat <patrik.jelic@uni-a.de>
2026-08-26 12:05:33 +0200

    Basic class for adaptive stepsize solvers

--- include/pyode/adaptive_solver.hpp
+++ include/pyode/adaptive_solver.hpp
@@ -3,8 +3,35 @@
 
 #include <sstream>
 
-class AdaptiveStepsizeSolver {
+#include "ode.hpp"
 
+enum AdaptiveStepsizeSolverType {
+    RK45
+};
+
+class AdaptiveStepsizeSolver {
+    public:
+        AdaptiveStepsizeSolver(AdaptiveStepsizeSolverType type, ODE ode);
+
+        void setInitialConditions(DoubleVector initial, double t0 = 0.0);
+        DoubleVector getCurrentSolution();
+        virtual void step(double max_stepsize) = 0; 
+
+        friend std::ostream& operator<<(std::ostream &strm, const AdaptiveStepsizeSolver& ass);
+
+    protected:
+        const AdaptiveStepsizeSolverType type;
+        const ODE ode;
+        DoubleVector current;
+        double t = 0.0;
+
+};
+
+class RK45Solver : public AdaptiveStepsizeSolver {
+    public:
+        RK45Solver(ODE ode);
+
+        void step(double max_stepsize) override;
 };
 
 #endif 
