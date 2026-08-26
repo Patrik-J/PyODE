b454cc0e1b588c20214baeb5c9aacf3dcf8ea8ff
jelicpat <patrik.jelic@uni-a.de>
2026-08-26 21:06:31 +0200

    Updated setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 
 ext_modules = [
     Pybind11Extension("pyode._core", 
-                      ["src/pyode/ode.cpp", "src/pyode/solver.cpp", "src/pyode/adaptive_solver.cpp", "src/pyode/more_math.cpp", "src/pyode/solver_exceptions.cpp", "src/pyode/_core.cpp"], 
-                      include_dirs=["include/pyode"],
+                      ["src/pyode/ode.cpp", "src/pyode/fixedstepsizesolver/solver.cpp", "src/pyode/adaptivestepsizesolver/adaptive_solver.cpp", "src/pyode/more_math.cpp", "src/pyode/solver_exceptions.cpp", "src/pyode/_core.cpp"], 
+                      include_dirs=["include/pyode", "include/pyode/adaptivestepsizesolver", "include/pyode/fixedstepsizesolver"],
                       cxx_std=17),
 ]
 
-setup(name="pyode", ext_modules=ext_modules, cmdclass={"build_ext": build_ext},
-      packages=["pyode"], package_dir={"": "src"})
+setup(name="pyode", ext_modules=ext_modules, cmdclass={"build_ext": build_ext}, packages=["pyode"], package_dir={"": "src"})
